b99c8d8c820ce8cd703ea7e51f27db0287523475
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2021-03-06 10:11:40 +0530

    Update main.py
    added support for taking inputs out of range, displaying winner

--- main.py
+++ main.py
@@ -10,19 +10,34 @@ while counter < 42:
 
     if counter % 2 == 1:
         #player1
-        c = int(input("enter column to drop coin: "))
-        board = dropCoin(board, c, player1)
+        print("\n\n------------------------------\nPlayer 1's Turn!\n")
         printNice(board)
-        if callCheck(board) != None:
-            print(callCheck(board))
-            break
+        c = int(input("Enter column to drop coin: "))
+        boardx = dropCoin(board, c, player1)
+        if boardx != None:
+            printNice(boardx)
+            board = boardx
+            if callCheck(board) != None:
+                print("Player %d is the winner!" % callCheck(board))
+                break
+        else:
+            print("Try again!")
+            counter -= 1
+        counter += 1
+
     if counter % 2 == 0:
         #player2
-        c = int(input("enter column to drop coin: "))
-        board = dropCoin(board, c, player2)
+        print("\n\n------------------------------\nPlayer 2's Turn!\n")
         printNice(board)
-        if callCheck(board) != None:
-            print(callCheck(board))
-            break
-    #check(board)
-    counter += 1
+        c = int(input("Enter column to drop coin: "))
+        boardx = dropCoin(board, c, player2)
+        if boardx != None:
+            printNice(boardx)
+            board = boardx
+            if callCheck(board) != None:
+                print("Player %d is the winner!" % callCheck(board))
+                break
+        else:
+            print("Try again!")
+            counter -= 1
+        counter += 1
