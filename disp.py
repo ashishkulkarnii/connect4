c878b1b815e05ec49439b5d5d4bee6e7fd2c8e13
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2021-03-16 20:11:31 +0530

    Update disp.py
    added column numbers

--- disp.py
+++ disp.py
@@ -1,3 +1,3 @@
 def printNice(board):
     board = [x for y in board for x in y]
-    print(" CONNECT 4!\n\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n".format(*board))
+    print(" CONNECT 4!\n\n"+chr(9312),chr(9313),chr(9314),chr(9315),chr(9316),chr(9317),chr(9318)+"\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n".format(*board))
