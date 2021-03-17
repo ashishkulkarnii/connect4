6378ba6710b8d3f0316d44111c641c760522cfbc
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2021-03-17 16:09:51 +0530

    Update disp.py
    added functions to make main.py more readable

--- disp.py
+++ disp.py
@@ -1,3 +1,27 @@
+from time import sleep
+from os import system, name 
+
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+
 def printNice(board):
     board = [x for y in board for x in y]
     print(" CONNECT 4!\n\n"+chr(9312),chr(9313),chr(9314),chr(9315),chr(9316),chr(9317),chr(9318)+"\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n{} {} {} {} {} {} {}\n".format(*board))
+    
+def loadingScreen():
+    print(" Loading.")
+    sleep(.2)
+    clear()
+    print(" Loading..")
+    sleep(.2)
+    clear()
+    print(" Loading...")
+    sleep(.2)
+    clear()
+    print(" Loading....")
+    sleep(.2)
+    clear()
+    print(" CONNECT 4!")
+    sleep(2)
+    clear()
